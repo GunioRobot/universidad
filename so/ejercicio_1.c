99fded05f6413ec20357011ea3d97f219be42f01
alejandrogomez <alejandroogomez@gmail.com>
2011-05-07 23:51:46 +0200

    ejercicios 1 y 2

--- so/ejercicio_1.c
+++ so/ejercicio_1.c
@@ -6,8 +6,8 @@
 #define NUEVO_FICHERO "nuevo_fichero.txt"
 
 /*
-/ Sin argumentos, imprime m·scara actual. Con un argumento,
-/ cambia la m·scara al valor dado y crea un fichero.
+/ Sin argumentos, imprime m√°scara actual. Con un argumento,
+/ cambia la m√°scara al valor dado y crea un fichero.
 */
 
 int mask_octal(char * num)
@@ -15,7 +15,7 @@ int mask_octal(char * num)
 	return (int) strtol(num, (char **) NULL, 8);
 }
 
-void create_fichero(char * name)
+void crear_fichero(char * name)
 {
 	open(name, O_CREAT | O_TRUNC);
 }
@@ -24,19 +24,23 @@ int main (int argc, char ** argv)
 {	
 	int nueva_mask;
 
+	if (argc > 2)
+	{
+		fprintf(stderr, "Uso: ejercicio1 <n√∫mero-en-octal>\n");
+		return;
+	}
+	
 	if (argc == 1)
 	{
-		printf("m·scara = %o\n", umask(0));
+		printf("m√°scara = %o\n", umask(0));
 	}
-	else if (argc == 2)
+	else 
 	{
 		nueva_mask = mask_octal(argv[1]);
-		printf("vieja m·scara = %o\n", umask(nueva_mask));
-		printf("nueva m·scara = %o\n", nueva_mask);
-		create_fichero(NUEVO_FICHERO);
+		printf("vieja m√°scara = %o\n", umask(nueva_mask));
+		printf("nueva m√°scara = %o\n", nueva_mask);
+		crear_fichero(NUEVO_FICHERO);
 	}
-	else
-		printf("Uso: ./ejercicio1 <n˙mero-en-octal>\n");
 	
 	return 0;
 }
---
+++ so/ejercicio_2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+
+/*
+/ Mueve el fichero especificado en el primer parámetro
+/ a la posición indicada por el segundo.
+*/
+
+// ejercicio_2 <fichero-origen> <fichero-destino>
+int main (int argc, char ** argv)
+{	
+	// TODO : usar link y unlink
+	
+	if (argc != 3)
+	{
+		printf("Uso: ejercicio1 <número-en-octal>\n");
+		return;
+	}
+	
+	char * origen = argv[1];
+	char * destino = argv[2];
+	
+	if (link(origen, destino) == -1)
+		fprintf(stderr, "Ha habido un error, %s debe existir.\n", origen);
+	
+	return 0;
+}
